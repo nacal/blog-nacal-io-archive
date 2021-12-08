ccf3ad7228878f4638fc1ca1657f7c59c30f181f
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-12-08 19:04:21 +0900

    develop common components base

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer className='w-full h-64 bg-gray-300'></footer>
+}
+
+export default Footer
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='grid place-items-center w-full h-16 bg-gray-300'>
+      <h1>blog.nacal.io</h1>
+    </header>
+  )
+}
+
+export default Header
