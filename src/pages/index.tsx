933ffa1b7d42bb124bca021f67deb42acba8cc2c
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-12-11 09:48:41 +0900

    clean code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,21 +27,19 @@ const Home: NextPage = () => {
   }, [])
 
   return (
-    <div>
+    <>
       <Head>
         <title>nacal.blog.io</title>
         <meta name='description' content='nacal.blog.io' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main>
-        <h1>nacal.blog.io</h1>
-        <p>{posts[0].fields.title}</p>
-        <p>{posts[0].fields.category.fields.name}</p>
-        <p>{posts[0].fields.publishedAt}</p>
-        <p>{posts[0].fields.body}</p>
-      </main>
-    </div>
+      <h1>nacal.blog.io</h1>
+      <p>{posts[0].fields.title}</p>
+      <p>{posts[0].fields.category.fields.name}</p>
+      <p>{posts[0].fields.publishedAt}</p>
+      <p>{posts[0].fields.body}</p>
+    </>
   )
 }
 
