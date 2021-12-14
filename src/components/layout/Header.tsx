1efa018d86db33b2efd17e4aeafebf1bdac6d3cf
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-12-15 02:09:25 +0900

    develop header link

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
+
 const Header = () => {
   return (
     <header className='grid place-items-center w-full h-16 bg-gray-300'>
-      <h1 className='font-en'>blog.nacal.io</h1>
+      <Link href='/' passHref>
+        <h1 className='font-en cursor-pointer'>blog.nacal.io</h1>
+      </Link>
     </header>
   )
 }
