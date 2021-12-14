d9ac49230126a3c895010231cd8306410dcc2ae7
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-12-15 01:59:57 +0900

    develop post page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,5 +1,47 @@
-const Post = () => {
-  return <p>yaa</p>
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
+import { createClient } from 'libs/contentful'
+
+const Post: NextPage = () => {
+  const [post, setPost] = useState<any>(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    ;(async () => {
+      const client = createClient()
+      try {
+        const res = await client
+          .getEntries({
+            content_type: 'blogPost',
+            order: '-sys.createdAt',
+            'sys.id': router.query.slug,
+          })
+          .catch((error: unknown) => {
+            return error
+          })
+        console.log('res', res.items)
+        setPost(res.items[0])
+      } catch (error) {
+        console.log(error)
+      }
+    })()
+  }, [])
+
+  return (
+    <>
+      {!post ? (
+        <></>
+      ) : (
+        <>
+          <h2>{post.fields.title}</h2>
+          <p>{post.fields.category.fields.name}</p>
+          <p>{post.fields.publishedAt.substr(0, 10)}</p>
+          <p>{post.fields.body}</p>
+        </>
+      )}
+    </>
+  )
 }
 
 export default Post
