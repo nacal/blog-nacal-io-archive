ab0aedea9fcef2ff7b32897d9f67c92bebed87f9
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-12-15 02:05:02 +0900

    develop card link

--- src/components/common/Card/Card.tsx
+++ src/components/common/Card/Card.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 type Props = {
   title: string
   category: string
@@ -8,15 +10,15 @@ type Props = {
 const Card: React.FC<Props> = (props) => {
   return (
     <article className='font-bold text-dark bg-light rounded-3xl'>
-      <a href={props.slug} className='cursor-pointer'>
-        <div className='flex flex-col justify-between p-4 w-64 h-64'>
+      <Link href={props.slug} passHref>
+        <div className='flex flex-col justify-between p-4 w-64 h-64 cursor-pointer'>
           <h2 className='text-4xl break-words'>{props.title}</h2>
           <div className='flex justify-between'>
             <p>{props.category}</p>
             <p>{props.publishedAt}</p>
           </div>
         </div>
-      </a>
+      </Link>
     </article>
   )
 }
